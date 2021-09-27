7150939f9b67d4367972ccffe5db9a62da6542ec
jsaylor5 <saylorj@live.com>
2021-09-27 15:11:43 -0400

    added dynamic grid input

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('#container');
 
-
-for (var i=0; i<256; i++){
+inputNum = 16;
+for (var i=0; i<inputNum; i++){
+    for (var a=0; a<inputNum; a++){
     const square = document.createElement('div');
     square.classList.add('square');
     square.textContent ='?';
     container.appendChild(square);
- };
+ }};
