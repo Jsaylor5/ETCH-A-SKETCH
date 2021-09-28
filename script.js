7d298c9b05c09e2c45731335056cde2a944ab1c8
jsaylor5 <saylorj@live.com>
2021-09-28 17:21:31 -0400

    added basic drawing function

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ function grid(inputNum) {
 for (var i=0; i<inputNum; i++){
     for (var a=0; a<inputNum; a++){
     const square = document.createElement('div');
-    square.classList.add('newSquare');
-    square.textContent ='?'; 
+    square.classList.add('newSquare'); 
     container.appendChild(square) 
- }};
+ }};};
+
+const hoverOver = document.querySelectorAll('.newSquare');
+for (const newSquare of hoverOver){
+    newSquare.addEventListener('mouseover',() =>{
+        newSquare.style.backgroundColor = "red";
+
+    })
 }
-
-const hoverOver = document.querySelector('.newSquare');
-
-
