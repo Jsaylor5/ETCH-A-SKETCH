56fb8568aa59b17010215192bbd8fbb5c1757c2f
jsaylor5 <saylorj@live.com>
2021-09-28 15:09:50 -0400

    fixed issue with grid and made hover over color

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('#container');
 
-inputNum = 16;
+grid(16);
+
+function grid(inputNum) {
 for (var i=0; i<inputNum; i++){
     for (var a=0; a<inputNum; a++){
     const square = document.createElement('div');
-    square.classList.add('square');
-    square.textContent ='?';
-    container.appendChild(square);
+    square.classList.add('newSquare');
+    square.textContent ='?'; 
+    container.appendChild(square) 
  }};
+}
+
+const hoverOver = document.querySelector('.newSquare');
+
+
