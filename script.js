fe0a2dfde8be7d12a7e8d2e4239d31163169f82d
jsaylor5 <saylorj@live.com>
2021-10-04 16:48:41 -0400

    Fixed new grid not coloring. Need to modify sizing of grid in container.

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 const container = document.querySelector('#container');
 
-grid()
+grid(16)
 
-function grid(inputNum=16) {
+function grid(inputNum) {
 for (var i=0; i<inputNum; i++){
     for (var a=0; a<inputNum; a++){
     const square = document.createElement('div');
     square.classList.add('newSquare'); 
     container.appendChild(square) 
- }};};
+ }};
+colorChange();
+};
 
-const hoverOver = document.querySelectorAll('.newSquare');
-for (const newSquare of hoverOver){
-    newSquare.addEventListener('mouseover',() =>{
-        newSquare.style.backgroundColor = "red";
+
+function colorChange(){
+    const hoverOver = document.querySelectorAll('.newSquare');
+    for (const newSquare of hoverOver){
+        newSquare.addEventListener('mouseover',() =>{
+            newSquare.style.backgroundColor = "red";
 
     })
-};
+};}
 
 let resetButton = document.querySelector('#reset')
 
 resetButton.onclick = function(){
     resetColor();
+    resetGrid();
     resize();
 }
 
 function resetColor(){
+    const hoverOver = document.querySelectorAll('.newSquare');
     for (const newSquare of hoverOver){
         newSquare.style.backgroundColor = "";
     }
 }
 
-//function not working
 function resize(){
     let size = window.prompt("Select size of grid 100 or under", 16);
     if (size < 101){
-        grid(size-inputNum);
+        grid(size);
     } else {
          text = "Select a size under 101";
  }
+}
+
+function resetGrid(){
+    const hoverOver = document.querySelectorAll('.newSquare');
+    hoverOver.forEach(square => square.remove());
 }
