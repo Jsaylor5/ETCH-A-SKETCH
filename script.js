a952be782c6d5c92d8c1187f3910796549850369
jsaylor5 <saylorj@live.com>
2021-10-07 01:20:09 -0400

    Added turn to black function

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 const container = document.querySelector('#container');
 
-grid(16)
+grid(16, randomColor)
 
-function grid(inputNum) {
-for (var i=0; i<inputNum; i++){
-    for (var a=0; a<inputNum; a++){
+function grid(inputNum, inputColor) {
+for (let i=0; i<inputNum; i++){
+    for (let a=0; a<inputNum; a++){
     const square = document.createElement('div');
     square.classList.add('newSquare'); 
     container.appendChild(square) 
  }};
-colorChange();
-};
 
+if (inputColor == randomColor){
+    colorChange();
+}
+else if (inputColor == greyColor){
+    greyColor();
+}
+};
 
 function colorChange(){
     const hoverOver = document.querySelectorAll('.newSquare');
     for (const newSquare of hoverOver){
         newSquare.addEventListener('mouseover',() =>{
             newSquare.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+        
 
     })
 };};
 
-let resetButton = document.querySelector('#reset')
+function greyColor(){
+    const hoverOver = document.querySelectorAll('.newSquare');
+    for (const newSquare of hoverOver){
+        newSquare.addEventListener('mouseover',() => {
+         
+        if(newSquare.dataset.color == 0){
+            newSquare.dataset.color = 0
+        }else if (newSquare.dataset.color <= 90 && newSquare.dataset.color >=10){
+            newSquare.dataset.color -= 10 
+        }else{
+            newSquare.setAttribute('data-color', 90);
+        }
+        console.log(newSquare.dataset.color)
+        newSquare.style.backgroundColor = `hsl(${randomHue()}, 100%, ${newSquare.dataset.color}%)`;
+        })
+    }
+};
 
-resetButton.onclick = function(){
-    resetColor();
+
+let colorButton = document.querySelector('#color')
+colorButton.onclick = () => {
+    grid(resize, randomColor)
+};
+
+let greyButton = document.querySelector('#grey')
+greyButton.onclick = () => {
+    grid(resize, greyColor)
+};
+
+let resetButton = document.querySelector('#reset')
+resetButton.onclick = () => {
     resetGrid();
     resize();
 };
 
-function resetColor(){
-    const hoverOver = document.querySelectorAll('.newSquare');
-    for (const newSquare of hoverOver){
-        newSquare.style.backgroundColor = "";
-    }
-};
 
 function resize(){
     let size = window.prompt("Select size of grid 100 or under", 16);
@@ -43,13 +70,14 @@ function resize(){
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
     if (size < 101){
-        grid(size);
+        grid(size, randomColor);
     } else {
          alert("Select a size under 101");
          container.style.gridTemplateColumns = null;
          container.style.gridTemplateRows = null;
          grid(16);
  }
+ return size
 };
 
 function resetGrid(){
@@ -61,3 +89,9 @@ function randomColor(){
     color = Math.floor(Math.random()*255);
     return color;
 };
+
+function randomHue(){
+    color = Math.floor(Math.random()*360);
+    return color;
+};
+
